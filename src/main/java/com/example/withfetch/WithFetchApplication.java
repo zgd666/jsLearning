b207bd8b1f2e8d6e20ebdf871903060ca34d2905
zhangguoding <guoding.zhang@enovell.com.cn>
2023-12-28 15:37:30 +0800

    初始化提交

---
+++ src/main/java/com/example/withfetch/WithFetchApplication.java
@@ -0,0 +1,13 @@
+package com.example.withfetch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithFetchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithFetchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/withfetch/WithFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.withfetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithFetchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
